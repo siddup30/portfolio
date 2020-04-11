381568b42f09dda061f3933ce3f46579161f7044
siddu <siddu.patil@searchlighthealth.com>
2020-04-12 00:21:58 +0530

    initial commit

---
+++ precache-manifest.ee2834fc1f9a72fd551a837d3c87600d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d7fd6c28922d30c3e6779ded313b306",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73f87165be225632e315",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "631b3c5119ed2ddae1a8",
+    "url": "/static/css/main.85b9bd11.chunk.css"
+  },
+  {
+    "revision": "73f87165be225632e315",
+    "url": "/static/js/2.9cb7b423.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9cb7b423.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631b3c5119ed2ddae1a8",
+    "url": "/static/js/main.ffb8db1b.chunk.js"
+  },
+  {
+    "revision": "81f4cb9e78ea9076776a",
+    "url": "/static/js/runtime-main.c035942e.js"
+  },
+  {
+    "revision": "031569a142030324dfc319405bb801d1",
+    "url": "/static/media/bg-main.031569a1.jpg"
+  },
+  {
+    "revision": "66606ed707d6e8158a7ec44328de142e",
+    "url": "/static/media/mypicture.66606ed7.jpeg"
+  }
+]);
